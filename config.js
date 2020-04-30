dd51902d55247e95bef6acbf9782f711c2844955
Dan Valinotti <dvalinotti@icloud.com>
2020-04-30 11:01:38 -0400

    First commit

---
+++ config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  pathPrefix: '',
+  siteUrl: 'https://gabrielslevin.com',
+  siteTitle: 'Gabriel Levin',
+  siteDescription: 'Mechanical Engineering Graduate Portfolio',
+  author: 'Gabriel Levin',
+  postsForArchivePage: 3,
+  defaultLanguage: 'en',
+  pages: {
+    home: '/',
+    blog: 'blog',
+    contact: 'contact',
+    resume: 'resume',
+  },
+  social: {
+    linkedin: 'https://www.linkedin.com',
+    rss: '/rss.xml',
+  },
+  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
+  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
+  tags: {
+    angular: {
+      description: 'Angular is an open source web application platform.',
+    },
+    electron: {
+      description: 'Electron is a framework for building cross-platform desktop applications with web technology.',
+    },
+    javascript: {
+      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
+    },
+    laravel: {
+      description: 'Laravel is a PHP framework for building web applications following the MVC pattern.',
+    },
+    nodejs: {
+      name: 'Node.js',
+      description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
+    },
+    rxjs: {
+      name: 'RxJS',
+      description: 'RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.',
+    },
+    sass: {
+      description: 'Sass is a stable extension to classic CSS.',
+    },
+    typescript: {
+      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+    },
+    react: {
+      description: 'React is an open source JavaScript library used for designing user interfaces.',
+    },
+    vuejs: {
+      name: 'Vue.js',
+      description: 'Vue.js is a JavaScript framework for building interactive web applications.',
+    },
+  },
+};
---
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import AboutTile from '../../AbouTile';
+import { stripTags, domHtml } from '../../../utils/stripTags';
+import SEO from '../../Seo';
+
+const pageText = {
+  paraOne: `I am a final semester senior graduating this May of 2020 from the New Jersey Institute of Technology 
+  majoring in Mechanical Engineering Technology with a minor in Manufacturing Engineering. At NJIT, I am part of 
+  the Robotics and Drone Science lab as the Mechanical Designer solving problems for the research lab that involve 
+  creative ways to help the robots complete their job when programming simply will not do the trick. I am also part 
+  of the Greek community excelling in providing examples on how one can exceed scholarly and socially.`,
+  paraTwo: `With my past two semesters consisting of greater than 3.8 GPA and a highly successful summer internship with a 
+  leading generic pharmaceutical company as a Production Engineer, I can proudly say that I am excited to enter the 
+  field with impressive skills and experience to offer.`,
+};
+const AboutMe = () => {
+  const description = `${pageText.paraOne} ${stripTags(pageText.paraTwo)}`;
+  return (
+    <>
+      <div>
+        <SEO
+          title="About"
+          description={description}
+          path=""
+          keywords={['Rolwin', 'Reevan', 'Monteiro', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby']}
+        />
+        <h1 className="titleSeparate">About Me</h1>
+        <p>
+          {pageText.paraOne}
+        </p>
+        <p dangerouslySetInnerHTML={domHtml(pageText.paraTwo)} />
+      </div>
+      <Row gutter={[20, 20]}>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="location.png"
+            height={60}
+            alt="location image"
+            textH4="Born and bought up in"
+            textH3="Mangalore, KA, India"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="coffee.png"
+            alt="coffee image"
+            textH4="Love Coffee"
+            textH3="Coffee + Me = Happiness"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="meeting.png"
+            alt="meeting image"
+            textH4="Socially Awkward"
+            textH3="At times"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="motorcycle.png"
+            alt="motorcycle image"
+            textH4="Love Riding"
+            textH3="Biker for life"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="web.png"
+            alt="web image"
+            textH4="Self Taught Programmer"
+            textH3="Thanks to the Web Resources"
+            height={60}
+            width={60}
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="graduation.png"
+            alt="graduation image"
+            textH4="Pursued B.Tech in"
+            textH3="Computer Science"
+            height={60}
+            width={60}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+export default AboutMe;
